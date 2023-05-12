9a4a048175319dcc15ff8da8d1ccc17e7e98d1ef
Oleksandr Kondratov <19samsung81k9@gmail.com>
2023-05-12 15:34:41 +0300

    dist removed from tracking

--- dist/routes/cabinetRouter.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { closeCabinets, openCabinets, } from "../controllers/cabinetController.js";
-const cabinetRouter = Router();
-cabinetRouter.route("/open").post(openCabinets);
-cabinetRouter.route("/close").post(closeCabinets);
-export default cabinetRouter;
